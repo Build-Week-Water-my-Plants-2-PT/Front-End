7ea42711ddbbe27e8f90e2ba784ed2d2c7de68eb
Mitchell Wright <mitchellbwright@gmail.com>
2020-09-24 19:19:33 -0600

    try axios defaults

--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 export const axiosWithAuth = () => {
   const token = localStorage.getItem("token");
+  axios.defaults.withCredentials = true;
 
   return axios.create({
     headers: {
